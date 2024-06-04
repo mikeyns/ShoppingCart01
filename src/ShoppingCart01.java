83c29efb15eb5b71eb2b94154062426dd2e56f2e
Aluno <Aluno@10.85.198.42>
2024-06-04 17:11:01 -0300

    java project ShoppingCart01

---
+++ src/ShoppingCart01.java
@@ -0,0 +1,16 @@
+public class ShoppingCart01 {
+    public static void main(String[] args) {
+        // Declare and initialize String variables.  Do not initialize message yet.
+        String custName = "Alex";
+        String itemDesc = "Camisa";
+        System.out.println (custName + " quer comprar uma " + itemDesc);
+        
+        
+        
+        // Assign the message variable 
+        
+        
+        // Print and run the code
+        
+    }
+}
